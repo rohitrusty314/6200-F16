608fec9aba392ac7f8743ccd6d3144f8de3e6780
Rohit Raj <rohit314@ccs.neu.edu>
2016-10-05 15:17:19 -0400

    Assignment 1 Crawlers

---
+++ 2_Focused_BFS_Crawler.py
@@ -0,0 +1,94 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import time
+uni_list = []
+depth_list = []
+
+def url_before_hash(url):
+    hash_at = url.find("#")
+    url_substr = url[0:hash_at]
+    return url_substr
+
+def get_url_list(crawlurl):
+    curr_urllist = []
+    time.sleep(1)
+    urlhandler = urlopen(crawlurl)
+    html = urlhandler.read()
+
+    soup = BeautifulSoup(html)
+    i=1
+    for a in soup.find_all('a',href=True):
+        if a['href'].startswith("/wiki/") and\
+           a['href'].find(":") == -1 and\
+           a['href'].find("Main_Page") == -1 and\
+           ("solar" in a['href'].lower() or "solar" in a.text.lower()):
+
+            if a['href'].find("#") != -1:
+                a['href'] =  url_before_hash(a['href'])
+                
+            insert_url = True
+            for url in curr_urllist:
+                if a['href'] == url:
+                    insert_url = False
+
+            if insert_url:
+                curr_urllist.append(a['href'])
+                i+=1
+    return curr_urllist
+
+        
+def depth2_onwards(depth):
+    for i in range(depth_list[2*depth-1],depth_list[2*depth]):
+        print(depth+2," ", depth_list[2*depth-1], " ", depth_list[2*depth]," ",len(uni_list))
+        
+        if len(uni_list) >= 1000:
+            return
+        new_url_list = get_url_list("https://en.wikipedia.org"+uni_list[i])
+        for url in new_url_list:
+            url_insert = True
+            for uni_url in uni_list:
+                if url == uni_url:
+                    url_insert = False
+            if url_insert:
+                uni_list.append(url)
+        
+        
+
+seed = "https://en.wikipedia.org/wiki/Sustainable_energy"
+uni_list.append("/wiki/Sustainable_energy")
+prev_length = 1
+new_url_list = get_url_list(seed)
+for url in new_url_list:
+    url_insert = True
+    for uni_url in uni_list:
+        if url == uni_url:
+            url_insert = False
+    if url_insert:
+        uni_list.append(url)
+
+depth_list.append(0)
+
+for i in range(1,4):
+    if len(uni_list) >= 1000:
+            break
+    depth_list.append(prev_length)
+    depth_list.append(len(uni_list))
+    prev_length = depth_list[2*i]
+    depth2_onwards(i)
+
+upper_url_count = 1000
+
+if(len(uni_list) < 1000):
+    upper_url_count = len(uni_list)
+
+crawler_file = open("2_Focused_Url_list.txt","w")
+
+for i in range(0,upper_url_count):
+    print(i," ",uni_list[i])
+    crawler_file.write("https://en.wikipedia.org"+uni_list[i])
+    crawler_file.write("\n")
+
+crawler_file.close()
+print(len(uni_list))
+
+
